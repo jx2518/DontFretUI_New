a85297827831f62f17f8eafe9b4694ab77110c9f
Tom Xu <jx2518@columbia.edu>
2024-12-10 18:14:24 -0500

    made it so that we can customize items in the scrollable.

--- Assets/SpectaclesInteractionKit/Components/UI/ScrollView/GridContentCreator.ts
+++ Assets/SpectaclesInteractionKit/Components/UI/ScrollView/GridContentCreator.ts
@@ -1,25 +1,52 @@
 @component
 export class GridContentCreator extends BaseScriptComponent {
+    @input
+    itemPrefab: ObjectPrefab;
 
-  @input
-      itemPrefab: ObjectPrefab;
-  @input
-      itemsCount: number = 10;
+    @input
+    itemsCount: number = 10;
 
-  onAwake() {
-      if (!this.itemPrefab) {
-          throw new Error('ItemPrefab is not wired in SceneObject:' +
-          this.getSceneObject().name);
-      }
+    @input
+    itemNames: string[]; // Input for item names
 
-      const yStart = 0;
-      const yOffset = -5.4;
+    onAwake() {
+        if (!this.itemPrefab) {
+            throw new Error(
+                'ItemPrefab is not wired in SceneObject:' + this.getSceneObject().name
+            );
+        }
 
-      for (let i = 0; i < this.itemsCount; i++) {
-          const item = this.itemPrefab.instantiate(this.getSceneObject());
-          const screenTransform = item.getComponent('Component.ScreenTransform');
-          screenTransform.offsets.setCenter(new vec2(0, yStart + yOffset * i));
-          item.enabled = true;
-      }
-  }
+        const yStart = 0;
+        const yOffset = -5.4;
+
+        for (let i = 0; i < this.itemsCount; i++) {
+            const item = this.itemPrefab.instantiate(this.getSceneObject());
+            const screenTransform = item.getComponent('Component.ScreenTransform');
+
+            if (screenTransform) {
+                screenTransform.offsets.setCenter(
+                    new vec2(0, yStart + yOffset * i)
+                );
+            }
+
+            item.enabled = true;
+
+            // Manually find the ContactName object
+            let contactName = null;
+            for (let j = 0; j < item.getChildrenCount(); j++) {
+                const child = item.getChild(j);
+                if (child.name === 'ContactName') { // Use .name instead of getName()
+                    contactName = child;
+                    break;
+                }
+            }
+
+            if (contactName) {
+                const textComponent = contactName.getComponent('Component.Text');
+                if (textComponent && this.itemNames && i < this.itemNames.length) {
+                    textComponent.text = this.itemNames[i]; // Assign the name
+                }
+            }
+        }
+    }
 }
