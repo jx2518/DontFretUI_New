49b44b98a954fa9a12ee801bc98c0c48093f2eb9
GitGoblr <jhegstad12@gmail.com>
2024-12-12 20:36:37 -0500

    aaaa

--- Assets/buttontoggle.js
+++
@@ -1,8 +0,0 @@
-//@input Component.ScriptComponent toggleScript
-//@input SceneObject sceneObject
-
-function switchObjectOnOff(toggleValue) {
-  script.sceneObject.enabled = toggleValue;
-}
-
-script.toggleScript.onToggle.add(switchObjectOnOff);
